664a8e6bc7fbc91c95c7c0e2f75080b3a75b347f
Mohammed Nadershah <mohammed.a.nadershah@gmail.com>
2021-11-08 11:11:06 +0300

    Practice PHP

--- detail.php
+++ detail.php
@@ -1,14 +1,38 @@
-<div class="row">
-<div class="col-md-6 col-lg-4">
-<div class="card">
-  <img src="https://services.hanselandpetal.com/photos/california_snow.jpg" class="card-img-top" alt="california_snow.jpg">
-  <div class="card-body">
-    <h5 class="card-title">Shrubs</h5>
-    <h5 class="card-subtitle">price: 15.99,</h5>
-    <p class="card-text">"instructions": "Large double. Good grower, heavy bloomer. Early to mid-season, acid loving plants. Plant in moist well drained soil with pH of 4.0-5.5.",
-      </p>
-    
-  </div>
+<?php 
+  require("inc/header_inc.php");
+  require("flowers.php");
+  define("BASED_URL", "https://services.hanselandpetal.com/photos/");
+  $print = true;
+
+	if(isset($_GET["id"])){
+		$flowers_object = new Flowers();
+		$flower_prdoduct_id = $_GET["id"];
+		$flower = $flowers_object->getFlower($flower_prdoduct_id);
+		if(sizeof($flower) == 0){
+			$print = false;
+		}
+	}else{
+		$print = false;
+	}
+  
+?>
+<?php if($print): ?>
+<div class="container-fluid">
+	<div class="row d-flex justify-content-center g-1 p-3">
+		<div class="col-md-6 col-lg-4">
+			<div class="card">
+					<img src="<?php echo BASED_URL . $flower["photo"];?>" class="card-img-top" alt="<?php echo $flower["photo"]; ?>">
+					<div class="card-body">
+					<h5 class="card-title"><?php echo $flower["name"];?></h5>
+					<h5 class="card-subtitle">price: <?php echo $flower["price"]; ?></h5>
+					<p class="card-text"><?php echo $flower["instructions"];?></p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-</div>
+<?php endif; ?>
+
+<?php
+  require("inc/footer_inc.php");
+?>
